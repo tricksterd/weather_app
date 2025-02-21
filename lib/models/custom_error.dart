ed9cbfb63feb86c451f580bc6cca3a4da78c6309
tricksterd <kozavkolena@gmail.com>
2025-02-21 15:18:02 +0200

    created models

---
+++ lib/models/custom_error.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+class CustomError extends Equatable {
+  final String errorMessage;
+
+  const CustomError({this.errorMessage = ''});
+
+  @override
+  List<Object?> get props => [
+        errorMessage,
+      ];
+}
