b0cee6d18bbcb2f6d1cffd5368f0aa234948ad0c
tricksterd <kozavkolena@gmail.com>
2025-02-21 17:02:02 +0200

    added weather repositiry

---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,39 @@
+import '../exceptions/weather_exception.dart';
+import '../models/custom_error.dart';
+import '../models/direct_geocoding.dart';
+import '../models/weather.dart';
+import '../services/weather_api_services.dart';
+
+class WeatherRepository {
+  final WeatherApiServices weatherApiServices;
+
+  WeatherRepository({
+    required this.weatherApiServices,
+  });
+
+  Future<Weather> fetchWeather(String city) async {
+    try {
+      final DirectGeocoding directGeocoding =
+          await weatherApiServices.getDirectGeocoding(city);
+      print(directGeocoding);
+
+      final Weather tempWeather =
+          await weatherApiServices.getWeather(directGeocoding);
+
+      final Weather weather = tempWeather.copyWith(
+        name: directGeocoding.name,
+        country: directGeocoding.country,
+      );
+
+      return weather;
+    } on WeatherException catch (e) {
+      throw CustomError(
+        errorMessage: e.message,
+      );
+    } catch (e) {
+      throw CustomError(
+        errorMessage: e.toString(),
+      );
+    }
+  }
+}
