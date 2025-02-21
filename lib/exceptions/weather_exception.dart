a80304b70d88ecc4f212cb29e657b18bd632cd2f
tricksterd <kozavkolena@gmail.com>
2025-02-21 16:54:29 +0200

    added weather api service

---
+++ lib/exceptions/weather_exception.dart
@@ -0,0 +1,9 @@
+class WeatherException implements Exception {
+  String message;
+  WeatherException([this.message = 'Something went wrong']) {
+    message = 'Weather Exception: $message';
+  }
+
+  @override
+  String toString() => message;
+}
