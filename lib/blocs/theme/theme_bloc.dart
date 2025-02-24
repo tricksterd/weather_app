3165a49a080edebb23780719f5377beca109993c
tricksterd <kozavkolena@gmail.com>
2025-02-24 20:28:56 +0200

    deleted dead code

--- lib/blocs/theme/theme_bloc.dart
+++ lib/blocs/theme/theme_bloc.dart
@@ -1,32 +1,13 @@
-import 'dart:async';
-
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../constants/constants.dart';
-import '../weather/weather_bloc.dart';
 
 part 'theme_event.dart';
 part 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
-  // final WeatherBloc weatherBloc;
-  // late final StreamSubscription _weatherSubscription;
-  ThemeBloc(
-      //   {
-      //   required this.weatherBloc,
-      // }
-      )
-      : super(ThemeState.initial()) {
-    // _weatherSubscription = weatherBloc.stream.listen(
-    //   (WeatherState weatherState) {
-    //     if (weatherState.weather.temp > kWarmOrNot) {
-    //       add(const SetThemeEvent(appTheme: AppTheme.light));
-    //     } else {
-    //       add(const SetThemeEvent(appTheme: AppTheme.dark));
-    //     }
-    //   },
-    // );
+  ThemeBloc() : super(ThemeState.initial()) {
     on<SetThemeEvent>((event, emit) {
       emit(state.copyWith(appTheme: event.appTheme));
     });
@@ -39,10 +20,4 @@ class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
       add(const SetThemeEvent(appTheme: AppTheme.dark));
     }
   }
-
-  // @override
-  // Future<void> close() {
-  //   _weatherSubscription.cancel();
-  //   return super.close();
-  // }
 }
