b76c06fc43f0589df9e575e0ec34ece8f5de03c4
tricksterd <kozavkolena@gmail.com>
2025-02-24 10:35:55 +0200

    added city search, displaying information and crossplatform error_dialog

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recase/recase.dart';
+
+import '../constants/constants.dart';
+import '../cubits/weather/weather_cubit.dart';
+import '../widgets/error_dialog.dart';
+import 'search_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,15 +15,184 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? _city;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              _city = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const SearchPage(),
+                ),
+              );
+              print('$_city');
+              if (_city != null) {
+                context.read<WeatherCubit>().fetchWeather(_city!);
+              }
+            },
+            icon: const Icon(
+              Icons.search,
+            ),
+          )
+        ],
       ),
-      body: const Center(
-        child: Text('Home'),
+      body: _showWeather(context),
+    );
+  }
+
+  String showTemperature(double temperature) {
+    return '${temperature.toStringAsFixed(2)} ℃';
+  }
+
+  Widget showIcon(String icon) {
+    return FadeInImage.assetNetwork(
+      placeholder: 'assets/images/loading.gif',
+      image: 'http://$kIconHost/img/wn/$icon@4x.png',
+      width: 96,
+      height: 96,
+    );
+  }
+
+  Widget formatText(String description) {
+    final formattedString = description.titleCase;
+    return Text(
+      formattedString,
+      style: const TextStyle(
+        fontSize: 24.0,
       ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _showWeather(BuildContext context) {
+    return BlocConsumer<WeatherCubit, WeatherState>(
+      listener: (context, state) {
+        if (state.status == WeatherStatus.error) {
+          errorDialog(
+            context,
+            state.error.errorMessage,
+          );
+        }
+      },
+      builder: (context, state) {
+        if (state.status == WeatherStatus.initial) {
+          return const Text(
+            'Select a city',
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          );
+        }
+        if (state.status == WeatherStatus.loading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (state.status == WeatherStatus.error && state.weather.name == '') {
+          return const Text(
+            'Select a city',
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          );
+        }
+        return ListView(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+            Text(
+              state.weather.name,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  TimeOfDay.fromDateTime(state.weather.lastUpdated)
+                      .format(context),
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  '(${state.weather.country})',
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 60.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  showTemperature(state.weather.temp),
+                  style: const TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  width: 20.0,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      showTemperature(state.weather.tempMax),
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      showTemperature(state.weather.tempMin),
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Spacer(),
+                showIcon(state.weather.icon),
+                Expanded(
+                  flex: 3,
+                  child: formatText(state.weather.description),
+                ),
+                const Spacer(),
+              ],
+            )
+          ],
+        );
+      },
     );
   }
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -8,14 +8,77 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? _city;
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+
+  void _submit() {
+    setState(() {
+      autoValidateMode = AutovalidateMode.always;
+    });
+
+    final form = _formKey.currentState;
+
+    if (form != null && form.validate()) {
+      form.save();
+      Navigator.pop(context, _city!.trim());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Search'),
       ),
-      body: const Center(
-        child: Text('Search'),
+      body: Form(
+        key: _formKey,
+        autovalidateMode: autoValidateMode,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 60.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30.0,
+              ),
+              child: TextFormField(
+                autofocus: true,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                ),
+                decoration: const InputDecoration(
+                  labelText: 'City name',
+                  hintText: 'more than 2 characters',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? input) {
+                  if (input == null || input.trim().length < 2) {
+                    return 'City name must be at least 2 characters long';
+                  }
+                  return null;
+                },
+                onSaved: (String? input) {
+                  _city = input;
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: _submit,
+              child: const Text(
+                'How\'s weather?',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
